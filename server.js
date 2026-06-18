159adc8b5b35c3bd626110d7b381fbb3c22f332a
BedBreaking <167460152+BedBreaking@users.noreply.github.com>
2026-06-18 07:49:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,74 +1,44 @@
-// server.js
-
+const http = require("http");
 const { WebSocketServer } = require("ws");
 
-const wss = new WebSocketServer({ port: 8080 });
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer();
+
+const wss = new WebSocketServer({
+  server,
+});
 
 const rooms = {};
 
 wss.on("connection", (socket) => {
   console.log("Player Connected");
+
   socket.on("message", (message) => {
-    console.log("Received:", JSON.parse(message));
     const data = JSON.parse(message);
+
     switch (data.type) {
       case "join":
-        joinRoom(socket, data?.payload);
+        joinRoom(socket, data.payload);
         break;
 
       case "move":
-        sendMove(socket, data?.payload);
+        sendMove(socket, data.payload);
         break;
     }
   });
 
   socket.on("close", () => {
     console.log("Player Disconnected");
-  });
-});
 
-function joinRoom(socket, data) {
-  console.log(data, "Data");
-  const { roomId, name } = data;
-  console.log(roomId, "Room ID");
-  console.log(name, "Player Name");
-  if (!rooms[roomId]) {
-    rooms[roomId] = [];
-  }
-
-  rooms[roomId].push(socket);
-  socket.roomId = roomId;
-  socket.name = name;
-
-  console.log(`Player joined room ${roomId}`);
-
-  socket.send(
-    JSON.stringify({
-      type: "joined",
-      roomId,
-      name: name,
-    }),
-  );
-  socket;
-}
-
-function sendMove(socket, data) {
-  console.log(rooms[socket.roomId], "room");
-  const room = rooms[socket.roomId];
-  if (!room) return;
-  room.forEach((player) => {
-    console.log(player, "player");
-    if (player !== socket) {
-      player.send(
-        JSON.stringify({
-          type: "move",
-          x: data.x,
-          y: data.y,
-          name: data.player,
-        }),
+    if (socket.roomId && rooms[socket.roomId]) {
+      rooms[socket.roomId] = rooms[socket.roomId].filter(
+        (player) => player !== socket,
       );
     }
   });
-}
+});
 
-console.log("WebSocket running on port 8080");
+server.listen(PORT, () => {
+  console.log(`WebSocket Server Running on ${PORT}`);
+});
