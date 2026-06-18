9af85e99fb3e3745d5885e00a8dffd0d5d238e6d
SarvoParsadMohanty <sarvoparsadmohanty72786@gmail.com>
2026-06-18 08:25:26 +0530

    Fix Server

--- server.js
+++ server.js
@@ -39,6 +39,53 @@ wss.on("connection", (socket) => {
   });
 });
 
+function joinRoom(socket, data) {
+  console.log(data, "Data");
+  const { roomId, name } = data;
+  console.log(roomId, "Room ID");
+  console.log(name, "Player Name");
+  if (!rooms[roomId]) {
+    rooms[roomId] = [];
+  }
+
+  rooms[roomId].push(socket);
+  socket.roomId = roomId;
+  socket.name = name;
+
+  console.log(`Player joined room ${roomId}`);
+
+  socket.send(
+    JSON.stringify({
+      type: "joined",
+      roomId,
+      name: name,
+    }),
+  );
+  socket;
+}
+
+function sendMove(socket, data) {
+  console.log(rooms[socket.roomId], "room");
+
+  const room = rooms[socket.roomId];
+  if (!room) return;
+
+  room.forEach((player) => {
+    console.log(player, "player");
+
+    if (player !== socket) {
+      player.send(
+        JSON.stringify({
+          type: "move",
+          x: data.x,
+          y: data.y,
+          name: data.player,
+        }),
+      );
+    }
+  });
+}
+
 server.listen(PORT, () => {
   console.log(`WebSocket Server Running on ${PORT}`);
 });
